da74604faf6648b62f41d9e5f7ca36040c175dc3
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-28 02:38:12 +0330

    Made the model and superuser

--- api/models.py
+++ api/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Article(models.Model):
-    title   = models.CharFiled(max_length=250) 
+    title   = models.CharField(max_length=250) 
     text    = models.TextField() 
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
