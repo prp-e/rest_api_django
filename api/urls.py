2c8a4a9226b2f3b09d3f1b899c2fa22100138b6b
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-28 03:04:50 +0330

    Commited API endpoints

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, include
 from rest_framework import routers 
 from . import views
 
+router = routers.DefaultRouter()
+router.register('', views.ArticleView)
+
 urlpatterns = [
-   
+   path('v1', include(router.urls))
 ]
