e6a43abf3942f5cf75d376705f6fdba24b20ae9d
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-28 02:42:36 +0330

    Added models to admin panel

--- api/models.py
+++ api/models.py
@@ -6,3 +6,7 @@ class Article(models.Model):
     title   = models.CharField(max_length=250) 
     text    = models.TextField() 
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title 
+    
