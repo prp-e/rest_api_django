fef9e813080c3bfe4a53a869d55233da188f3a9a
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-28 02:36:59 +0330

    Added Article model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User 
 # Create your models here.
+
+class Article(models.Model):
+    title   = models.CharFiled(max_length=250) 
+    text    = models.TextField() 
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
