0144667b6ca141eeed673e0a01ba9a9c44eb865a
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-29 05:44:39 +0330

    User serializer added

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers 
+from django.contrib.auth.models import User 
 from .models import Article 
 
-class ArticleSerializer(serializers.ModelSerializer): 
+class ArticleSerializer(serializers.HyperlinkedModelSerializer): 
     class Meta: 
         model = Article 
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User 
+        fields = (
+            "id", 
+            "username", 
+        )
--- api/views.py
+++ api/views.py
@@ -1,9 +1,14 @@
 # from django.shortcuts import render
 from rest_framework import viewsets 
+from django.contrib.auth.models import User 
 from .models import Article
-from .serializers import ArticleSerializer
+from .serializers import ArticleSerializer, UserSerializer
 
 # Create your views here.
 class ArticleView(viewsets.ModelViewSet): 
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer 
+
+class UserView(viewsets.ModelViewSet): 
+    queryset = User.objects.all() 
+    serializer_class = UserSerializer
