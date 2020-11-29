ee274b05d8afdca52e9927b72b316b5fd4724980
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-29 06:12:57 +0330

    Token paths added

--- api/urls.py
+++ api/urls.py
@@ -9,4 +9,6 @@ router.register('users', views.UserView)
 
 urlpatterns = [
    path('v1/', include(router.urls))
+   path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+	path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
